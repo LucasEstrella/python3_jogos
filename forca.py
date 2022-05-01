3b570ef2ee5373e8770ddf33d3520c6e9a0fd700
Lucas Estrella <lucasestrella01@gmail.com>
2022-05-01 11:49:37 -0300

    Implementando melhorias para o código e mostrando o loop relativa a List COmprehension

--- forca.py
+++ forca.py
@@ -3,27 +3,41 @@ def jogar():
     print("***Bem vindo ao jogo de Forca***")
     print("********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    #letras_acertadas = []
+    #for letra in palavra_secreta:
+    #    letras_acertadas.append("_")
 
     enforcou = False
     acertou = False
+    erros =  0
 
     print(letras_acertadas)
 
     while (not enforcou and not acertou):
 
         chute = input("Qual letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        posicao = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[posicao] = letra
-            posicao = posicao + 1
+        if(chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[posicao] = letra
+                posicao += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if(acertou):
+        print("Você ganhou!!!")
+    else:
+        print("Você perdeu!!!")
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
